6d9b536d664393bf67606c7ee67508853c421af1
ysolis-ias <ysolis@integralads.com>
2023-01-04 19:27:21 -0600

    yan's solution

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './StarRating.css';
 
-const StarRating = () => {
+const StarRating = ({number}) => {
+
+  const [activeStars, setActiveStars] = useState([])
+
+  const handleStars = (num) => {
+    let newArr = {}
+    for(let i=0; i<number; i++) {
+      if (i <= num) {
+        newArr[i] = true
+      } else {
+        newArr[i] = false
+      }
+    }
+    setActiveStars(Object.entries(newArr))
+  }
+
+  useEffect(() => {
+    let stars = {}
+    for(let i=0; i<number; i++ ){
+      stars[i] = false
+    }
+    
+    setActiveStars(Object.entries(stars))
+  },[number])
 
   return (
-      <div className='StarRating'> </div>
+      <div className='StarRating'>
+        {(activeStars.length > 0) ? activeStars?.map((e,i) => {
+          return <span key={i} onClick={() => handleStars(i)} className={(e[1]) ? 'active': ''}>*</span>
+        }) : null}
+      </div>
   );
 };
 
